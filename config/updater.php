c1e601f9c0422e7e9792e26a1487b8dac734cbf5
julians22 <dabnerjulian@gmail.com>
2022-07-15 00:44:48 +0700

    update again

---
+++ config/updater.php
@@ -0,0 +1,11 @@
+<?php
+
+    return [
+        'owner' => env('APP_OWNER', 'julians22'),
+        'repository' => env('APP_REPOSITORY', 'skripsi'),
+        'branch' => env('APP_BRANCH', 'master'),
+        'remote' => env('APP_REMOTE', 'origin'),
+        'github_token' => env('GITHUB_TOKEN', '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'),
+    ]
+
+?>
